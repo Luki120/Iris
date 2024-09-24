e40f051dcc74aad9123e207f3e74802ac2cae228
luki120 <luki120@exerra.xyz>
2024-09-24 06:16:18 -0300

    smh

--- Iris/Sources/Core/SceneDelegate.swift
+++ Iris/Sources/Core/SceneDelegate.swift
@@ -10,21 +10,21 @@ import UIKit
 @UIApplicationMain
 final class SceneDelegate: UIResponder, UIApplicationDelegate, UIWindowSceneDelegate {
 
-    var window: UIWindow?
+	var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = scene as? UIWindowScene else { return }
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+		guard let windowScene = scene as? UIWindowScene else { return }
 
-        let navBarAttributes = [NSAttributedString.Key.font: UIFont.quicksand(withStyle: .medium, size: 17)]
+		let navBarAttributes = [NSAttributedString.Key.font: UIFont.quicksand(withStyle: .medium, size: 17)]
 		let navBarLargeTitleTextAttributes = [NSAttributedString.Key.font: UIFont.quicksand(withStyle: .semiBold, size: 25)]
-        let tabBarItemAttributes = [NSAttributedString.Key.font: UIFont.quicksand(withStyle: .bold, size: 10)]
+		let tabBarItemAttributes = [NSAttributedString.Key.font: UIFont.quicksand(withStyle: .bold, size: 10)]
 
-        UINavigationBar.appearance().titleTextAttributes = navBarAttributes
+		UINavigationBar.appearance().titleTextAttributes = navBarAttributes
 		UINavigationBar.appearance().largeTitleTextAttributes = navBarLargeTitleTextAttributes
-        UITabBarItem.appearance().setTitleTextAttributes(tabBarItemAttributes, for: .normal)
+		UITabBarItem.appearance().setTitleTextAttributes(tabBarItemAttributes, for: .normal)
 
-        window = UIWindow(windowScene: windowScene)
-        window?.tintColor = .irisSlateBlue
+		window = UIWindow(windowScene: windowScene)
+		window?.tintColor = .irisSlateBlue
 		window?.makeKeyAndVisible()
 
 		Task {
