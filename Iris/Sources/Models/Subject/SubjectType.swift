129fe0d2699b3fbd896256cd2dff3fc44a2eb390
luki120 <luki120@exerra.xyz>
2025-08-01 00:32:08 -0300

    added new `SubjectType` model

---
+++ Iris/Sources/Models/Subject/SubjectType.swift
@@ -0,0 +1,28 @@
+//
+//  SubjectType.swift
+//  Iris
+//
+//  Created by Luki on 30/07/2025.
+//
+
+/// Enum useful for mapping `Subject` data to UI elements
+enum SubjectType {
+	case hye, physiology, twoExams, threeExams
+
+	init(subject: Subject) {
+		switch subject.name {
+			case "HyE": self = .hye
+			case "Physiology": self = .physiology
+			default: self = subject.hasThreeExams ? .threeExams : .twoExams
+		}
+	}
+
+	var exams: [String] {
+		switch self {
+			case .hye: return ["Biología", "Genética", "Histología", "Embriología", "Final"]
+			case .physiology: return ["R2", "R1", "Final"]
+			case .twoExams: return ["Primer parcial", "Segundo parcial", "Final"]
+			case .threeExams: return ["Primer parcial", "Segundo parcial", "Tercer parcial", "Final"]
+		}
+	}
+}
